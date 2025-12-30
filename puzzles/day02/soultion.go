7652b991083a9429544060555de77379ffc1f5e4
Krzysztof Kasprzyk <rskrobotics@gmail.com>
2025-12-30 22:14:06 +0100

    math solution

--- puzzles/day02/soultion.go
+++ puzzles/day02/soultion.go
@@ -2,24 +2,25 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
 type IDRange struct {
-	S, E int
+	s, e int
 }
 
 func ReadInput(path string) []IDRange {
 	file, err := os.ReadFile(path)
-	r := []IDRange{}
+	var r []IDRange
 	if err != nil {
 		panic(err)
 	}
-	splits := strings.Split(strings.TrimSpace(string(file)), ",")
+	splits := strings.SplitSeq(strings.TrimSpace(string(file)), ",")
 
-	for _, v := range splits {
+	for v := range splits {
 		parts := strings.Split(v, "-")
 		S, _ := strconv.Atoi(parts[0])
 		E, _ := strconv.Atoi(parts[1])
@@ -29,18 +30,28 @@ func ReadInput(path string) []IDRange {
 	return r
 }
 
+func isDoubleNumber(i int) bool {
+	digits := 0
+	for t := i; t > 0; t /= 10 {
+		digits++
+	}
+	if digits%2 == 1 || digits == 0 {
+		return false
+	}
+	divisor := int(math.Pow10(digits / 2))
+	firstHalf := i / divisor
+	secondHalf := i % divisor
+
+	return firstHalf == secondHalf
+}
+
 func main() {
 	ret := 0
 	ranges := ReadInput("inputs/input-2.txt")
 	fmt.Printf("Ranges %v\n", ranges)
 	for _, v := range ranges {
-		for i := v.S; i <= v.E; i++ {
-			evaluated := strconv.Itoa(i)
-			splitter := len(evaluated) / 2
-			if len(evaluated)%2 == 1 {
-				continue
-			}
-			if evaluated[:splitter] == evaluated[splitter:] {
+		for i := v.s; i <= v.e; i++ {
+			if isDoubleNumber(i) {
 				ret += i
 			}
 		}
